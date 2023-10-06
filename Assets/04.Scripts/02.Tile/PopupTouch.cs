ffa76c732c6bf412d93e861c3b6d33765e858ee5
장다훈 <jangdahoon8850@gmail.com>
2023-10-06 17:00:17 +0900

    파일 정리

---
+++ Assets/04.Scripts/02.Tile/PopupTouch.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class PopupTouch : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    void Click()
+    {
+
+    }
+
+
+}
